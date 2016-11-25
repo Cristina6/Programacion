fd800feb40aebe75aa7771187975d617e9500318
Cristina6 <cristina.etxaniz96@gmail.com>
2016-11-25 09:33:18 +0100

    Coche

---
+++ Coche.java
@@ -0,0 +1,39 @@
+
+public class Coche {
+	//atributos
+	private String nBastidor;
+	private String matricula;
+	private String marca;
+	//metodos
+	
+	public void arrancar(){
+		System.out.println("En marcha");
+	}
+	public String getnBastidor() {
+		return nBastidor;
+	}
+	public void setnBastidor(String nBastidor) {
+		this.nBastidor = nBastidor;
+	}
+	public String getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public void girar(String sentido){
+		
+	}
+	public void pitar(int miliSeg){
+		
+	}
+	
+	
+
+}
